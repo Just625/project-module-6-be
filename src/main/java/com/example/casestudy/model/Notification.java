b01ff30c51e3eac437385ba4268d0e161eac1339
longle2812 <lehailong2812@gmail.com>
2021-08-08 21:19:08 +0700

    add interactions Entity

---
+++ src/main/java/com/example/casestudy/model/Notification.java
@@ -0,0 +1,20 @@
+package com.example.casestudy.model;
+
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class Notification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String content;
+    private String link;
+    private boolean isRead;
+}
---
+++ src/main/java/com/example/casestudy/model/SingerInteraction.java
@@ -0,0 +1,25 @@
+package com.example.casestudy.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Data
+@Entity
+public class SingerInteraction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long senderId;
+    private Long recieverId;
+    private Long singerId;
+    private String comment;
+    private Date createdAt;
+    private String link;
+    private boolean likes;
+    private boolean isRead;
+}
